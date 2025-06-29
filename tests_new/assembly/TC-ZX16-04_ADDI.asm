745f4dc0c84f94e65f7dee65b554c06ef6406817
salmawaleed055 <salmawaleed055@aucegypt.edu>
2025-06-30 01:19:12 +0300

    Clean-01
    Cleaned the Repo from extra test cases.

--- tests_new/assembly/TC-ZX16-04_ADDI.asm
+++
@@ -1,6 +0,0 @@
-# TC-ZX16-04_ADDI.s
-# Test Objective: Validate I-type ADDI instruction
-# x1 = x1 + 7 (10 + 7 = 17)
-li x1, 10
-addi x1, 7
-ecall 0x3FF
--- tests_new/assembly/TC-ZX16-14_SB.asm
+++
@@ -1,7 +0,0 @@
-# TC-ZX16-14_SB.s
-# Test Objective: Validate SB (Store Byte) instruction
-# mem[a0+2] = x1[7:0]
-li x1, 0x0105 # Data for SB: x1 = 0x0105 (byte to store is 0x05)
-li a0, 0x1000 # Base address: a0 = 0x1000
-sb x1, 2(a0)  # Store byte 0x05 from x1 at mem[0x1000 + 2] = mem[0x1002]
-ecall 0x3FF
