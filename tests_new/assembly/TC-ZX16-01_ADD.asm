833a5c317322c1e19b2a294f5ae192b686013518
MahinourAbdelgawad <mahinourabdelgawad@aucegypt.edu>
2025-06-29 19:19:52 +0300

    ran assembler on 1 test case

--- tests_new/assembly/TC-ZX16-01_ADD.asm
+++ tests_new/assembly/TC-ZX16-01_ADD.asm
@@ -1,7 +1,8 @@
-# TC-ZX16-01_ADD.s
+# TC-ZX16-01_ADD.asm
 # Test Objective: Validate R-type ADD instruction
 # x1 = x1 + x2 (5 + 3 = 8)
+.text
 li x1, 5
 li x2, 3
 add x1, x2
-ecall 0x3FF
+ecall 0x3FF=
